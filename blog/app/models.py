0088a4cfdb4da0b9803601a17bc48f1041a86599
Shazroid <dr.denosumab@gmail.com>
2019-09-22 08:20:02 +0300

    coding of frontend and showw all posts

--- blog/app/models.py
+++ blog/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django .utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    post_name = models.CharField(max_length=100)
+    post_body = models.TextField(default="")
+    email = models.EmailField()
+    post_date = models.DateTimeField(default=timezone.now)
+    post_update = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=False)
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.post_name
+    
+    class Meta:
+        ordering = ('-post_date',)
--- blog/app/views.py
+++ blog/app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from app .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'app/base.html', {})
+    posts= Post.objects.all()
+    context= {
+        'title': "الرئيسيـــة",
+        'posts': posts,
+    }
+    
+    return render(request, 'app/index.html', context)
